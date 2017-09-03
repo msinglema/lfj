3dbcbc7616d8bf162766cff2a93868365e5a46f0
savioryu <yumanyang@163.com>
2017-09-03 19:06:14 +0800

    首页轮播组件替换
    vue-awesome-swiper

--- front/src/components/banner.vue
+++ front/src/components/banner.vue
@@ -1,24 +1,25 @@
 <style lang="less">
-.el-carousel__container{
-  min-height:2.4rem;
-  .el-carousel__item{
-    img{
-      width:100%;
-      height:auto;
+.swiper-container{
+    .image-item{
+      height: 3.0rem;
+      background-size: contain;
+      background-position: center;
+      background-repeat: no-repeat;
     }
-  }
 }
 </style>
 
 <template>
-    <el-carousel indicator-position="inside" height="100px">
-        <el-carousel-item v-for="(item,index) in features" :key="index">
-            <img :src="item.sImgUrl" :alt="item.sOprTitle">
-        </el-carousel-item>
-    </el-carousel>
+    <swiper :loadtheme="true" :options="swiperOption">
+        <swiper-slide v-for="(item, index) in features" :key="index">
+            <div class="image-item" :style="{backgroundImage:'url(' + item.sImgUrl + ')'}"></div>
+        </swiper-slide>
+         <div class="swiper-pagination" slot="pagination"></div>  
+    </swiper>
 </template>
 
 <script>
+import {swiper,swiperSlide} from 'vue-awesome-swiper'
 import { mapGetters, mapState, mapActions } from 'vuex'
 
 export default {
@@ -28,7 +29,13 @@ export default {
           {src: 'http://www.liangfangji.com/mockcdn/image/banner-1.jpg', alt:'1'},
           {src: 'http://www.liangfangji.com/mockcdn/image/banner-2.jpg', alt:'2'},
           {src: 'http://www.liangfangji.com/mockcdn/image/banner-3.jpg', alt:'3'}
-        ]
+        ],
+       swiperOption: {
+            pagination: '.swiper-pagination',
+            slidesPerView: 'auto',
+            paginationClickable: true,
+            spaceBetween:10
+        }
       }
     },
     computed:{
@@ -39,6 +46,10 @@ export default {
     },
     created (){
       this.getFeatures()
+    },
+    components:{
+       swiper,
+       swiperSlide
     }
 }
 </script>
